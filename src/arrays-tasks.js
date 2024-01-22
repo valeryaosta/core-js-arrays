003cf5a94477780906be83cd686486ba77e05c0d
valeryaosta <gerasimenko.valery@gmail.com>
2024-01-23 01:13:46 +0400

    feat: add solution fot the task createChunks

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -342,8 +342,10 @@ function calculateBalance(arr) {
  *    createChunks(['a', 'b', 'c', 'd', 'e'], 2) => [['a', 'b'], ['c', 'd'], ['e']]
  *    createChunks([10, 20, 30, 40, 50], 1) => [[10], [20], [30], [40], [50]]
  */
-function createChunks(/* arr, chunkSize */) {
-  throw new Error('Not implemented');
+function createChunks(arr, chunkSize) {
+  return Array.from({ length: Math.ceil(arr.length / chunkSize) }, (_, i) =>
+    arr.slice(i * chunkSize, i * chunkSize + chunkSize)
+  );
 }
 
 /**
